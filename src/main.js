4b812c33423a1e74bbd77182933532224bf4d71f
Andrew Giang <giang184@gmail.com>
2021-07-02 14:45:36 -0700

    add features to allow user to convert from any currency to any other currency

--- src/main.js
+++ src/main.js
@@ -5,42 +5,47 @@ import './css/styles.css';
 import CurrencyService from './currency-service.js';
 
 //clear input and output fields
-function clearFields() {
-  $('#amount').val("");
-  $('.showErrors').text("");
-  $('.showRates').text("");
-}
+// function clearFields() {
+//   $('#amount').val("");
+//   $('.showErrors').text("");
+//   $('.showRates').text("");
+// }
 
-function displayRates(response, amount, currency) {
+function displayRates(response, amount, currency1, currency2) {
   if (response.result === "success") {
-    if(response.conversion_rates[currency]) {
+    if(!response.conversion_rates[currency1] && !response.conversion_rates[currency2]) {
+      $('#showErrors').text(`Sorry, we don't have a conversion rate for ${currency1} or ${currency2}`);
+    } else if(!response.conversion_rates[currency1]) {
+      $('#showErrors').text(`Sorry, we don't have a conversion rate for ${currency1}`);
+    } else if (!response.conversion_rates[currency2]) {
+      $('#showErrors').text(`Sorry, we don't have a conversion rate for ${currency2}`);
+    } else {
       if(!amount) {
-        $('.showErrors').text(`Please enter a U.S. dollar amount`);
+        $('#showErrors').text(`Please enter a currency amount`);
       }
       else{
-        $('.showRates').text(`${amount} U.S. dollars is equal to ${(amount*response.conversion_rates[currency]).toFixed(2)} ${currency}`);
+        $('#showRates').text(`$${amount} ${currency1} is equal to $${(amount/response.conversion_rates[currency1]*response.conversion_rates[currency2]).toFixed(2)} ${currency2}`);
+        $('#outputAmount').html(`$${(amount/response.conversion_rates[currency1]*response.conversion_rates[currency2]).toFixed(2)}`);
+        $('#outputAmount').show();
       }
     }
-    else {
-      $('.showErrors').text(`Sorry, we don't have a conversion rate for ${currency}`);
-    }
   } else {
-    $('.showErrors').text(`There was an error: ${response}`);
+    $('#showErrors').text(`There was an error: ${response}`);
   }
 }
 
-//make the api call to fetch rates
-async function makeApiCall(amount, currency) {
+async function makeApiCall(amount, currency1, currency2) {
   const response = await CurrencyService.getCurrency();
-  displayRates(response, amount, currency);
+  displayRates(response, amount, currency1, currency2);
 }
 
 $(document).ready(function() {
   $('#convert').click(function(event) {
     event.preventDefault();
     const amount = $('#amount').val();
-    const currency = $("#currency").val();
-    clearFields();
-    makeApiCall(amount, currency);
+    const currency1 = $("#currency1").val();
+    const currency2 = $("#currency2").val();
+    // clearFields();
+    makeApiCall(amount, currency1, currency2);
   });
 });
